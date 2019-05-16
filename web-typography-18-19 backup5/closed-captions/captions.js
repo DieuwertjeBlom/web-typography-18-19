927a8ebd7647c8e884c58a92ee8048a9a025ca44
DieuwertjeBlom <31731568+DieuwertjeBlom@users.noreply.github.com>
2019-05-16 15:10:43 +0200

    heb hier een achtergrond achter de video gezet

---
+++ web-typography-18-19 backup5/closed-captions/captions.js
@@ -0,0 +1,39 @@
+//  
+captions[0] = ['0','0.525'];
+captions[1] = ['0.525','3.555'];
+captions[2] = ['3.555','6.635'];
+// or the ability to make objects fly.
+captions[3] = ['6.635','9.245'];
+captions[4] = ['9.575','11.705'];
+captions[5] = ['12.865','13.865'];
+// Don't forget the nice wrist movement, we've been practicing
+captions[6] = ['14.185','17.145'];
+captions[7] = ['17.145','18.62'];
+captions[8] = ['18.62','20.485'];
+// everyone
+captions[9] = ['20.485','21.485'];
+captions[10] = ['22.745','23.745'];
+captions[11] = ['24.355','25.355'];
+// Good. Oh and enuciate
+captions[12] = ['26.185','28.985'];
+captions[13] = ['28.985','32.385'];
+captions[14] = ['34.615','37.315'];
+// Wingardium Leviosar
+captions[15] = ['38.875','41.835'];
+captions[16] = ['43.165','46.205'];
+captions[17] = ['46.205','47.205'];
+// Besides you're saying it wrong.
+captions[18] = ['47.365','50.055'];
+captions[19] = ['50.055','53.535'];
+captions[20] = ['53.615','56.985'];
+// Wingardium Leviosa
+captions[21] = ['56.985','58.335'];
+captions[22] = ['58.705','61.595'];
+captions[23] = ['70.425','71.705'];
+// Oh well done! See here everyone miss Granger done it!
+captions[24] = ['72.125','75.145'];
+captions[25] = ['75.145','76.635'];
+captions[26] = ['77.02','80.14'];
+// Oh
+captions[27] = ['82.88','83.88'];
+captions[28] = ['85.56','89'];
